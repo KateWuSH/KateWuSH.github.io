f0853bb691dd3c1da98c3c060fa9c556c7844d3f
Kate Wu <kate.allwin@gmail.com>
2017-12-05 18:15:45 +0800

    upload assets and upload

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    // home topBanner
+    $('.top-banner-pc-slick').slick({
+        autoplay: false,
+        dots: true,
+        infinite: true,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    $('.top-banner-mobile-slick').slick({
+        autoplay: false,
+        dots: true,
+        infinite: true,
+        fade: true,
+        cssEase: 'linear'
+    });
+})
