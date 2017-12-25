1cc09c3caae82c73a85036415cbf6120dc8340b5
Kate Wu <kate.allwin@gmail.com>
2017-12-25 11:11:34 +0800

    [service] animation

--- assets/js/service.js
+++ assets/js/service.js
@@ -1,37 +1,43 @@
 $(document).ready(function() {
 
-    $('#service .advantage .img').waypoint(function() {
-        $('.advantage .img').addClass('movein');
-    }, {
-        offset: '50%'
-    });
-
     $('#service .advantage').waypoint(function() {
-        $('.advantage .text').addClass('movein');
+        $('.advantage').addClass('movein');
     }, {
         offset: '50%'
     });
 
-    $('#service .innovation .text').waypoint(function() {
-        $('.innovation .text').addClass('movein');
+    $('#service .application .integration').waypoint(function() {
+        $('.application .integration').addClass('movein');
     }, {
-        offset: '70%'
+        offset: '50%'
     });
 
-    $('#service .items .item').waypoint(function() {
-        $('.items .item').addClass('movein');
+    $('#service .application .item01').waypoint(function() {
+        $('.application .item01').addClass('movein');
     }, {
-        offset: '70%'
+        offset: '50%'
     });
 
-    $('#service .integration').waypoint(function() {
-        $('.integration').addClass('movein');
+    $('#service .application .item02').waypoint(function() {
+        $('.application .item02').addClass('movein');
     }, {
-        offset: '60%'
+        offset: '50%'
     });
 
-    $('#service .future .target').waypoint(function() {
-        $('.future .target').addClass('movein');
+    $('#service .application .item03').waypoint(function() {
+        $('.application .item03').addClass('movein');
+    }, {
+        offset: '50%'
+    });
+
+    $('#service .innovation .wrapper').waypoint(function() {
+        $('.innovation .wrapper').addClass('movein');
+    }, {
+        offset: '50%'
+    });
+
+    $('#service .future').waypoint(function() {
+        $('.future').addClass('movein');
     }, {
         offset: '50%'
     });
